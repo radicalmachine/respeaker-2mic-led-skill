dba024011582c595f82d89525c4d867dced0687b
dony71 <dony70@gmail.com>
2020-06-12 18:02:17 -0700

    initial release

---
+++ GPIO.py
@@ -0,0 +1,135 @@
+"""This is test code for interacting with GPIO
+
+This is intented to mock up an interface into the RPi GPIO until hardware
+is accquired
+"""
+
+import json
+import threading
+import time
+
+try:
+    import RPi.GPIO as GPIO
+    """This is trapped so you can still run without RPi.GPIO
+
+    GPIO will be checked before use
+    """
+    pi_interface = True
+except:
+    pi_interface = False
+    pass
+
+
+blink_active = False
+"""bool: to controll weather blink continues to be active
+
+This will not start the blinking it will only be important once blinking
+has started.  This is currently only used in testing.
+"""
+
+GPIO_STATE = {}
+""" This is an object of tracking GPIO"""
+
+GPIO_ON = {}
+""" This maps functions to gpio activit
+
+These functions will be called when the GPIO is changed in any way.
+"""
+
+
+is_imported = True
+"""Used as a flag to show that GPIO was imported"""
+
+def ButtonHandeler(channel):
+    """This handels the button press and sets the state
+
+    Args:
+        channel(int):  The RPi GPIO channel used for monitoring the button
+    """
+    if GPIO.input(channel) == GPIO.HIGH:
+        set("Button","Pressed")
+    else:
+        set("Button","Released")
+
+"""This is the setup for the RPi GPIO"""
+if pi_interface:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(12,GPIO.OUT)
+    GPIO.setup(13,GPIO.OUT)
+    GPIO.setup(17,GPIO.IN)
+    GPIO.remove_event_detect(17)
+    GPIO.add_event_detect(17,GPIO.BOTH,ButtonHandeler)
+
+def set(key,value):
+    """This is used to set values for GPIO
+
+    This function is used to set values for each of GPIO's Will also call
+    the GPIO's function if it exisits.
+
+    Args:
+        key(int or str): Used to identify the gpio to interface
+        value(int or str): The value to set the gpio to.
+    """
+    GPIO_STATE[key] = value
+    if (key=="GPIO12") and pi_interface:
+        if value.upper()=="ON":
+            GPIO.output(12,GPIO.HIGH)
+        else:
+            GPIO.output(12,GPIO.LOW)
+    if (key=="GPIO13") and pi_interface:
+        if value.upper()=="ON":
+            GPIO.output(13,GPIO.HIGH)
+        else:
+            GPIO.output(13,GPIO.LOW)
+    if key in GPIO_ON:
+        GPIO_ON[key]()
+
+def get(key):
+    """ Returns the value of the givien GPIO
+
+    Args:
+        key(int or str): Used to identify the gpio to interface
+    """
+    return GPIO_STATE[key]
+
+def on(key,function):
+    """Used to set the function for the GPIO interface
+
+    Args:
+        key(int or str): Used to identify the gpio to interface
+        function(function): The function for the gpio
+    """
+    GPIO_ON[key]= function
+
+def json():
+    """Returns a json object of the GPIO info"""
+    return json.dumps(GPIO_STATE)
+
+
+if __name__=="__main__":
+    """This is console testing of the GPIO.  Not ment for automated testing
+
+    This should show that everything is working to the developer and allow
+    for changes going forward
+    """
+    def printgpio():
+        print(GPIO_STATE)
+
+    def blink_led():
+        if blink_active:
+            threading.Timer(0.5, blink_led).start()
+        if get("GPIO12")!="On":
+            set("GPIO12","On")
+        else:
+            set("GPIO12","Off")
+
+    on("GPIO12",printgpio)
+
+    set("GPIO12","On")
+    blink_active = True
+    blink_led()
+    time.sleep(10)
+    blink_active = False
+    if pi_interface:
+        print("GPIO is valid")
+
--- __init__.py
+++ __init__.py
@@ -20,11 +20,57 @@ from mycroft.util.log import LOG
 from mycroft import intent_file_handler
 from .pixels import Pixels
 
+from os.path import dirname, abspath
+import sys
+import requests
+import json
+import threading
+import GPIO
+sys.path.append(abspath(dirname(__file__)))
 
-class Respeaker2MicHatSkill(MycroftSkill):
+
+class Respeaker2MicLedSkill(MycroftSkill):
+
+    def on_led_change(self, gpio):
+        """used to report the state of the led.
+        This is attached to the on change event.  And will speak the
+        status of the led.
+        """
+        status = GPIO.get(gpio)
+        self.speak("Led is %s" % status)
+
+    def on_button_change(self):
+        status = GPIO.get("Button")
+        self.speak("Button is %s" % status)
 
     def __init__(self):
-        super(Respeaker2MicHatSkill, self).__init__(name="Respeaker2MicHatSkill")
+        self.blink_active = False
+        GPIO.on("GPIO12",self.on_led_change("GPIO12"))
+        GPIO.on("GPIO13",self.on_led_change("GPIO13"))
+        GPIO.on("Button",self.on_button_change)
+        super(Respeaker2MicLedSkill, self).__init__(name="Respeaker2MicLedSkill")
+
+    def blink_gpio12(self):
+        """This Will Start the Led blink process
+        This function will start the led blink process and continue
+        until blink_active is false.
+        """
+        if self.blink_active:
+            threading.Timer(10, self.blink_gpio12).start()
+        if self.blink_active:
+            if GPIO.get("GPIO12")!="On":
+                GPIO.set("GPIO12","On")
+            else:
+                GPIO.set("GPIO12","Off")
+
+    def blink_gpio13(self):
+        if self.blink_active:
+            threading.Timer(10, self.blink_gpio13).start()
+        if self.blink_active:
+            if GPIO.get("GPIO13")!="On":
+                GPIO.set("GPIO13","On")
+            else:
+                GPIO.set("GPIO13","Off")
 
     def initialize(self):
         self.pixels = Pixels()
@@ -32,6 +78,18 @@ class Respeaker2MicHatSkill(MycroftSkill):
         self.enable()
         self.pixels.wakeup()
         self.pixels.off()
+        """This function will initialize the Skill for Blinking an LED
+        This creates two intents
+            * IoCommandIntent - Will fire for any command that controlls the LED
+            * SystemQueryIntent - Will fire for any system command
+        The SystemQueryIntent was desinged for debug info while testing
+        and is not required going forward.
+        """
+        self.load_data_files(dirname(__file__))
+        command_intent = IntentBuilder("IoCommandIntent").require("command").require("ioobject").optionally("ioparam").build()
+        system_intent = IntentBuilder("SystemQueryIntent").require("question").require("systemobject").build()
+        self.register_intent(command_intent, self.handle_command_intent)
+        self.register_intent(system_intent, self.handle_system_intent)
 
     def enable(self):
         self.log.info("Pixel Ring: Enabling")
@@ -95,5 +153,89 @@ class Respeaker2MicHatSkill(MycroftSkill):
         self.disable()
         self.speak_dialog("DisablePixelRing")
 
+    def handle_system_intent(self, message):
+        """This is the handeler for system intent.
+        This will handle all questions of the system for debug info.
+        Args:
+            message(obj):
+                This is the object containing the message that fired the
+                intent.  This is used to discover what to do within the
+                intent.
+        """
+        if message.data["systemobject"] == "Name":
+            self.speak_dialog("name")
+            self.speak(__name__)
+        elif message.data["systemobject"] == "GPIO":
+            self.speak_dialog("check")
+            if GPIO.is_imported:
+                self.speak("GPIO is Imported")
+            else:
+                self.speak("GPIO is not Imported")
+        elif message.data["systemobject"] == "Modules":
+            self.speak_dialog("modules")
+            for module in sys.modules:
+                self.speak(module)
+        elif message.data["systemobject"] == "Path":
+            self.speak_dialog("path")
+            for path in sys.path:
+
+    def handle_command_intent(self, message):
+        """This will handle all command intents for controlling GPIO
+        This handles all commands to controll the LEDS including checking
+        the status.
+        Args:
+            message(obj):
+                This is the object containing the message that fired the
+                intent.  This is used to discover what to do within the
+                intent.
+        """
+        if message.data["command"].upper() == "BLINKING":
+            if message.data["ioobject"].upper() == "RED LED":
+                self.speak_dialog("redledblink")
+                if self.blink_active:
+                    self.blink_active = False
+                else:
+                    self.blink_active = True
+                    self.blink_gpio12()
+            if message.data["ioobject"].upper() == "GREEN LED":
+                self.speak_dialog("greenledblink")
+                if self.blink_active:
+                    self.blink_active = False
+                else:
+                    self.blink_active = True
+                    self.blink_gpio13()
+            if message.data["ioparam"].upper() == "STOP":
+                self.stop()
+        elif message.data["command"].upper() == "STATUS":
+            if message.data["ioobject"].upper() == "RED LED":
+                self.on_led_change("GPIO12")
+            if message.data["ioobject"].upper() == "GREEN LED":
+                self.on_led_change("GPIO13")
+        elif message.data["command"].upper() == "TURN":
+            if message.data["ioobject"].upper() == "RED LED":
+                if "ioparam" in message.data:
+                    if message.data["ioparam"].upper() == "ON":
+                        self.blink_active = False
+                        GPIO.set("GPIO12","On")
+                    elif message.data["ioparam"].upper() == "OFF":
+                        self.blink_active = False
+                        GPIO.set("GPIO12","Off")
+                else:
+                    self.speak_dialog("ipparamrequired")
+            if message.data["ioobject"].upper() == "GREEN LED":
+                if "ioparam" in message.data:
+                    if message.data["ioparam"].upper() == "ON":
+                        self.blink_active = False
+                        GPIO.set("GPIO13","On")
+                    elif message.data["ioparam"].upper() == "OFF":
+                        self.blink_active = False
+                        GPIO.set("GPIO13","Off")
+                else:
+                    self.speak_dialog("ipparamrequired")
+
+    def stop(self):
+        """This function will clean up the Skill"""
+        self.blink_active = False
+
 def create_skill():
-	return Respeaker2MicHatSkill()
+	return Respeaker2MicLedSkill()
